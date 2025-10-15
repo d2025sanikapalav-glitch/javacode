622b36b987015ac99bd5eac90152e465dec1fc07
d2025sanikapalav-glitch <d2025.sanika.palav@ves.ac.in>
2025-10-15 20:54:51 +0530

    Add files via upload

--- PrimeCheck.java
+++ PrimeCheck.java
@@ -3,15 +3,14 @@ import java.util.Scanner;
 public class PrimeCheck {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a number to check if it's prime: ");
+        System.out.print("Enter a number: ");
         int num = sc.nextInt();
-
         boolean isPrime = true;
 
         if (num <= 1) {
             isPrime = false;
         } else {
-            for (int i = 2; i <= Math.sqrt(num); i++) {
+            for (int i = 2; i <= num / 2; i++) {
                 if (num % i == 0) {
                     isPrime = false;
                     break;
@@ -23,5 +22,7 @@ public class PrimeCheck {
             System.out.println(num + " is a prime number.");
         else
             System.out.println(num + " is not a prime number.");
+
+        sc.close();
     }
 }
